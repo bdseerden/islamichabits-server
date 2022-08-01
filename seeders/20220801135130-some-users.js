41ac6e7d2a962319a92ce3492a76d9697bf3e275
BDSEERDEN <bodoekeleseerden@gmail.com>
2022-08-01 15:39:16 +0100

    Setup backend

---
+++ seeders/20220801135130-some-users.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Henry Ford",
+          email: "henry@ford.com",
+          phone: 3,
+          password: "helloworld",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
